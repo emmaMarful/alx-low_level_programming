1c10bd01c8774483983424c04a886ac79ad9c7e6
emmaMarful <eamarful@gmail.com>
2023-02-16 18:34:49 -0800

    101quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of a program
+ * Description: C program that prints without using puts and printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	fprintf(stdout, "and that piece of art is useful\" - ");
+	fprintf(stdout, "Dora Korpar, 2015-10-19\n");
+	return (1);
+}
