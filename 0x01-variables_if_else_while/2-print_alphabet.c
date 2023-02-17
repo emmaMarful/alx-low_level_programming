116aed338139a5c3547734b1da9552b66b6fb312
emmaMarful <eamarful@gmail.com>
2023-02-17 15:37:26 -0800

    printalphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: a program to print alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	int alphabet;
+
+	alphabet = 'a';
+	while (alphabet < 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}
