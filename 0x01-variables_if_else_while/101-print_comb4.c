182b24e21dfcd3b65686fb8cbb61a35f99e32a10
emmaMarful <eamarful@gmail.com>
2023-02-17 21:50:01 -0800

    101print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i, j, l;
 
-	for (i = '0'; j <= '9'; j++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (i = '0'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			for (l = '0'; l <= '9'; l++)
 			{
