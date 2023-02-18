c83cf1294472edd4e31ed0b854a4c7440fdb8f98
emmaMarful <eamarful@gmail.com>
2023-02-17 18:09:07 -0800

    9print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of a program
+ * Description: printing the possible combinations of single-digit numbers.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+		}
+		else
+		{
+			putchar('$');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
