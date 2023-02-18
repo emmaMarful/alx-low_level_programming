69141e2f54bd540826cd52a561ca5e544b4f67a5
emmaMarful <eamarful@gmail.com>
2023-02-17 16:59:44 -0800

    reverse_alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of  program
+ * Description: program to reverse the alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar("\n");
+	return (0);
+}
