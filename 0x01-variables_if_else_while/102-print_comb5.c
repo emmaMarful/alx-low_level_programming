e78e8eac075477e36582596d9c6248dff104d770
emmaMarful <eamarful@gmail.com>
2023-02-17 21:54:31 -0800

    102print

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - starting point
+ * Description: combination of 2-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+				if (((k + l) > (i + j) &&  k >= i) || i < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+
+					if (i + j + k + l == 227 && i == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
