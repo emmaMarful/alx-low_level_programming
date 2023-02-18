483141ef853debf90cba02468d3b71947c14b199
emmaMarful <eamarful@gmail.com>
2023-02-17 16:16:20 -0800

    printBase10_single

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/**
+ * main - starting point of a program
+ * Description: a program to single print digit numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
