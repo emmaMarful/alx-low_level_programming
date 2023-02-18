e8fc663b5e192b59b82ee99dccfa56416d7a25c9
emmaMarful <eamarful@gmail.com>
2023-02-17 17:40:10 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of a program
+ * Description: outputting hexadecimal
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+	char hex_letter;
+
+	num = 0;
+	hex_letter = 'a';
+	for (num = 0; num < 17; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		}
+		else if (num > 9)
+		{
+			putchar(hex_letter);
+			hex_letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
