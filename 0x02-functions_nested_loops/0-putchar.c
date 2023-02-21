f412056cf93f1068047a6eb69af48d1eb4855824
emmaMarful <eamarful@gmail.com>
2023-02-21 13:23:54 -0800

    puthcar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,9 @@ int main(void)
 	char p [] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int i;
 
-	for (i = 0; i < sizeof(p); i++)
+	for (i = 0; i <= sizeof(p); i++)
 	{
-		putchar(p[i]);
+		_putchar(p[i]);
 	}
 	putchar('\n');
 	return (0);
