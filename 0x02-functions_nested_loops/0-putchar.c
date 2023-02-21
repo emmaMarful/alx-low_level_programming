2f6e92c232bed632cd1cb079877b5e654a9046a7
emmaMarful <eamarful@gmail.com>
2023-02-21 12:36:48 -0800

    puthcar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/**
+ * main - starting point of a code
+ * Description: a program that print's _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	char p [] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int i;
+
+	for (i = 0; i < sizeof(p); i++)
+	{
+		putchar(p[i]);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-putchar.c - a script that prints _putchar, followed by a new line.
