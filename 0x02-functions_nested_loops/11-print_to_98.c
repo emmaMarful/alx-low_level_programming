e067a883f4c11d7aeadc9998fdef471022fb7381
emmaMarful <eamarful@gmail.com>
2023-02-21 19:21:14 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,21 +9,36 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
-		while (n < 98)
-		{
-			printf("%d, ", n);
-			n++;
-		}
-	}
-	else if (n > 98)
+	for (; n <= 98; n++)
 	{
-		while (n > 98)
-		{
-			printf("%d, ", n);
-			n--;
-		}
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
 	}
-	printf("98\n");
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
 }
