0726f99ed49aeccf74bb86ba7ed12eae29580bc4
emmaMarful <eamarful@gmail.com>
2023-02-21 17:05:34 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - function
+ * @n: argument of function _abs
+ * Description: a function to compute the absolute value of integer
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	int prt;
+
+	prt = abs(n);
+}
