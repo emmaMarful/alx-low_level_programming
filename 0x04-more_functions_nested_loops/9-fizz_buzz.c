08947beaa12e5ca48bbef55d0f754223af24c8d5
emmaMarful <eamarful@gmail.com>
2023-02-23 19:58:56 -0800

    fizzBuzz update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,19 @@ int main(void)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("FizzBuzz \n");
+			printf("FizzBuzz ");
 		}
 		else if (num % 3 == 0)
 		{
-			printf("Fizz \n");
+			printf("Fizz ");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz \n");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d \n", num);
+			printf("%d ", num);
 		}
 		num++;
 	}
