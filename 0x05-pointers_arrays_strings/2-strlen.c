7daaf9160d4725cb51966b913546290db047c67e
emmaMarful <eamarful@gmail.com>
2023-02-27 13:23:29 -0800

    2strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - is a function with a char type as argument
+ * @s: is a variable
+ * Description: a function that returns the length of a string
+ * Return: strlen(s)
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
