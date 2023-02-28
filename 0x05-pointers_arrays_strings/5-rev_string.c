d0c21dd01a8e6e64959a2f0e1d7d7d1af717c1d4
emmaMarful <eamarful@gmail.com>
2023-02-27 16:45:26 -0800

    revstring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - a function
+ * @s: an argument of the function rev_string
+ * Description: reverse
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int lenOfStr;
+	char temp;
+
+	lenOfStr = 0;
+	while (s[lenOfStr])
+	{
+		lenOfStr++;
+	}
+	for (int i = 0; i < lenOfStr; i++)
+	{
+		lenOfStr--;
+		temp = s[i];
+		s[i] = s[lenOfStr];
+		s[lenOfStr] = temp;
+	}
+}
