d2b42cf877f13cb737f9478e5a46413431962f65
emmaMarful <eamarful@gmail.com>
2023-02-27 18:39:24 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,20 +3,27 @@
 #include <math.h>
 #include <time.h>
 
+/**
+ * main - starting point of a program
+ * Description: a program to generate password
+ * Return: (0);
+ */
+
 int main(void)
 {
-	srand ((unsigned int)(time(NULL)));
+	srand((unsigned int)(time(NULL)));
 	int i;
-	char pass[12];
+	char pass[12], uppCase, lowerCase;
 
 	for (i = 0; i < 4; i++)
 	{
 		pass[i] = rand() % 9;
-		char uppCase = 'A' + (rand() % 26);
+		uppCase = 'A' + (rand() % 26);
 		pass[i + 2] = uppCase;
-		char lowerCase = 'a' + (rand() % 26);
+		lowerCase = 'a' + (rand() % 26);
 		pass[i + 3] = lowerCase;
 		printf("%d%c%c", pass[i], pass[1 + 2], pass[i + 2], pass[i + 3]);
 	}
 	printf("\n\n");
+	return (0);
 }
