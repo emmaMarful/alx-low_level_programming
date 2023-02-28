b5a6203c2f28212d69d4c9c3688dcd23aab7ed0f
emmaMarful <eamarful@gmail.com>
2023-02-27 16:57:15 -0800

    6puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - a function
+ * @str: an argument of function puts2
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int lenOfStr;
+
+	lenOfStr = 0;
+	while (str[lenOfStr])
+	{
+		if (lenOfStr % 2 == 0)
+		{
+			_putchar(str[lenOfStr]);
+		}
+		lenOfStr++;
+	}
+}
