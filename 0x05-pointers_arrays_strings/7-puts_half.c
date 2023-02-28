3dd65303dea3e89631e837e3b437d252ace9e50e
emmaMarful <eamarful@gmail.com>
2023-02-27 17:15:58 -0800

    putshalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - is a function
+ * @str: is an argument of function puts_half
+ * Description: a function that prints half ofa string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int lenOfStr, half;
+
+	lenOfStr = 0;
+	while (str[lenOfStr])
+	{
+		lenOfStr++;
+	}
+	for (half = lenOfStr / 2; str[half] != '\0'; half++)
+	{
+		_putchar(str[half]);
+	}
+	_putchar('\n');
+}
