fda3d057396e82703dba8d22b9bd42f3612fabe7
emmaMarful <eamarful@gmail.com>
2023-02-27 18:05:16 -0800

    arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_array - is a function
+ * @a: is  an argument
+ * @n: is an argument
+ * Description: Numbers must be separated by comma and space.
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int indexArr;
+
+	indexArr = 0;
+	while (n > 0)
+	{
+		printf("%d", a[indexArr]);
+		n--;
+		if (n > 0)
+		{
+			printf(", ");
+		}
+		indexArr++;
+	}
+}
