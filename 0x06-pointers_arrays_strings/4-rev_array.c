87609079be57f62040d3f8454d83c43c251a628c
emmaMarful <eamarful@gmail.com>
2023-03-02 07:32:15 -0800

    reverse gear

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - is a function
+ * @a: is an argument
+ * @n: is an argument
+ * Description: a function that reverse the content of an array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp = 0;
+
+	while (i < n)
+	{
+		n--;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		i++
+	}
+}
