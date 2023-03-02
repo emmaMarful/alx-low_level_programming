97190254978a8503da7cf744f177612619c5fbcf
emmaMarful <eamarful@gmail.com>
2023-03-02 04:09:17 -0800

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,20 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
-	return (dest);
+	j = 0;
+	while (src[j++])
+	{
+		lenOfSrc++;
+	}
+	for (i = lenOfSrc; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest)
 }
