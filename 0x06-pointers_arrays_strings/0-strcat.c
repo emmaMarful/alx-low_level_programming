87763d72fa846822d23d9ac14bd055ee2e53275b
emmaMarful <eamarful@gmail.com>
2023-03-01 19:11:17 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _stract - is a function
+ * @dest: ia an argument
+ * @src: is an argument
+ * Description: a function that concatenates two strings
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lenOfDest, indexOfArr;
+
+	lenOfDest = 0;
+	while (lenOfDest <= dest[len1])
+	{
+		lenOdDest++;
+	}
+	for (indexOfArr = 0; indexOfArr <= src[indexOfArr]; indexOfArr++)
+	{
+		dest[lenOfDest + indexOfArr] = src[indexOfArr];
+	}
+	return (dest);
+}
