3b1c5342c7bc67c1267bba2f7e1912f25fbc1bae
emmaMarful <eamarful@gmail.com>
2023-03-02 17:02:56 -0800

    3str

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] == s2[i])
-		{
-			continue;
-		}
-		break;
+	do {
+		s1++;
+		s2++;
 	}
+	while ((*s1 && *s2) && (*s1 || *s2))
+		;
+
 	return (*s1 - *s2);
 }
