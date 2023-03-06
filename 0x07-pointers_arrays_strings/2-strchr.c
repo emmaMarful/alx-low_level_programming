f93e7174e74dda11ee91f9b2c7a1e1e24e290327
emmaMarful <eamarful@gmail.com>
2023-03-05 20:24:07 -0800

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-	while (s[i])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == c)
+		if (s[i] != c)
 		{
-			break;
+			continue;
 		}
-		i++;
+		return (s + i);
 	}
-	return (s + i);
-}
+	return ('\0');
