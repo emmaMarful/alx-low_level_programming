22c7b05e5b70c9ff4be40d666d97a19b0fc6fcef
emmaMarful <eamarful@gmail.com>
2023-03-08 18:39:20 -0800

    reverse gear

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - a funcion that prints a string in reverse
+ * @s: output
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i])
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
