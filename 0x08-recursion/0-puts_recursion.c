92e3a82e25128af174b6663b239adeb660ae4020
emmaMarful <eamarful@gmail.com>
2023-03-08 18:33:24 -0800

    recure

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a funcion that prints a string
+ * @s: output
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[i])
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
