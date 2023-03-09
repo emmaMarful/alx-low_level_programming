3f855a0140047794c83472cd24bf2549e0b9221e
emmaMarful <eamarful@gmail.com>
2023-03-08 18:58:52 -0800

    power to

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - a function that returns the x to the power y
+ * @x: number
+ * @y: the power.
+ * Return: x to the power of y.
+ */
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x,y - 1));
+}
