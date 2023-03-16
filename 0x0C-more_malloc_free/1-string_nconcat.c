f26c6b18c6df0753d4b35f48613a31df8d2ec03d
emmaMarful <eamarful@gmail.com>
2023-03-16 16:40:44 -0700

    fix task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,38 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * *string_nconcat - a function that concatenates two strings
- * @s1: destination string
- * @s2: source, string to be concatenated
- * @n: size of byte in s2
- * Return: return if s1 and s2 is equal to null or return
- * null if dest is equal to null or return the pointer dest
+ * *string_nconcat - a function that adds two strings
+ * @s1: destination of string to be added
+ * @s2: string to be concatenated
+ * @n: length of string
+ * Return: if dest is equal to null return null or return pointer
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *dest;
-        unsigned int i, s1Len, dest_len = 0;
+	char *dest;
+	unsigned int i, s1Len = 0, dest_len = 0;
 
-        if (s1 == NULL)
-                s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-        if (s2 == NULL)
-                s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        for (i = 0; s1[i]; i++)
-                s1Len++;
+	for (i = 0; s1[i]; i++)
+		s1Len++;
 
-        dest = malloc(sizeof(char) * (s1Len + 1));
+	dest = malloc(sizeof(char) * (s1Len + 1));
 
-        if (string1 == NULL)
-                return (NULL);
+	if (string1 == NULL)
+		return (NULL);
 
-        for (i = 0; s1[i]; i++)
-                dest[dest_len++] = s1[i];
-
-        for (i = 0; i < n; i++)
-                dest[dest_len++] = s2[i];
-
-        dest[dest_len] = '\0';
-
-        return (dest);
+	for (i = 0; s1[i]; i++)
+		dest[dest_len++] = s1[i];
+	for (i = 0; i < n; i++)
+		dest[dest_len++] = s2[i];
+	dest[dest_len] = '\0';
+	return (dest);
 }
 
