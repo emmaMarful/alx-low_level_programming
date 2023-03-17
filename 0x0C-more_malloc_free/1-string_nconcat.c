182d325be5eff92bf1a677c0fb917dadc65fdd97
emmaMarful <eamarful@gmail.com>
2023-03-16 17:18:59 -0700

    asd

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 /**
- * *string_nconcat - a function that adds two strings
- * @s1: destination of string to be added
+ * *string_nconcat - this is a function that adds two strings
+ * @s1: destination
  * @s2: string to be concatenated
- * @n: length of string
- * Return: if dest is equal to null return null or return pointer
+ * @n: length of string 2
+ * Return: dest
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *dest;
-	unsigned int len = n, i, dest_len = 0;
+	unsigned int len = n, i, dlen = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -23,18 +23,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; i++)
 		len++;
 
-	dest = malloc((len + 1) * sizeof(char));
+	dest = malloc(sizeof(char) * (len + 1));
 
 	if (dest == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
-		dest[dest_len++] = s1[i];
+		dest[dlen++] = s1[i];
 
 	for (i = 0; s2[i] && i < n; i++)
-		des[dest_len++] = s2[i];
+		dest[dlen++] = s2[i];
 
-	dest[des_len] = '\0';
+	destt[dlen] = '\0';
 
 	return (dest);
 }
