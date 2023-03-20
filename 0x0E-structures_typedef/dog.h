468f5672d9d0e6deb829650611b4c0e6a391421e
emmaMarful <eamarful@gmail.com>
2023-03-20 13:17:44 -0700

    setup

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a structure type of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: woner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
